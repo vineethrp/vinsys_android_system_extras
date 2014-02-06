f3d14330515073b306603890dd7c948f0ac9b48c
Vineeth Pillai <vineethrp@gmail.com>
2014-02-05 22:09:32 -0800

    Initial commit to vinsys repo

--- su/Android.mk
+++ su/Android.mk
@@ -1,3 +1,5 @@
+ifndef SUPERUSER_EMBEDDED
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -13,3 +15,4 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := debug
 
 include $(BUILD_EXECUTABLE)
+endif
